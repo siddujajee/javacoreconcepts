2afcfd524d0c0ce41cbcd1940301340a336ba63a
siddujajee <jajeesiddu2002@gmail.com>
2025-06-05 01:10:46 +0530

    inverted right angle triangle

---
+++ src/main/java/com/corejava/patterns/RightPositionedInvertedTriangle.java
@@ -0,0 +1,15 @@
+package com.corejava.patterns;
+
+public class RightPositionedInvertedTriangle {
+    public static void main(String[] args) {
+        for(int i=1;i<=5;i++){
+            for(int k=1;k<=i;k++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=5-i+1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
