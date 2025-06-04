16dae0cbd7cde431057e6aa757e14c9acb52ec6c
siddujajee <jajeesiddu2002@gmail.com>
2025-06-05 01:20:44 +0530

    pyramid pattern

---
+++ src/main/java/com/corejava/patterns/InvertedPyramid.java
@@ -0,0 +1,15 @@
+package com.corejava.patterns;
+
+public class InvertedPyramid {
+    public static void main(String[] args) {
+        for(int i=1;i<=5;i++){
+            for(int k=1;k<=i;k++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=(2*(5-i+1)+1);j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
