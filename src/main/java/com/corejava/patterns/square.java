3a5394d69500dd0c3fecd1c0268743a37784f1a3
siddujajee <jajeesiddu2002@gmail.com>
2025-06-05 00:33:08 +0530

    condition changes for square pattern

--- src/main/java/com/corejava/patterns/square.java
+++ src/main/java/com/corejava/patterns/square.java
@@ -2,8 +2,8 @@ package com.corejava.patterns;
 
 public class square {
     public static void main(String[] args) {
-        for(int i=0;i<4;i++){
-            for(int j=0;j<4;j++){
+        for(int i=1;i<=4;i++){
+            for(int j=1;j<=4;j++){
                 System.out.print("*");
             }
             System.out.println();
