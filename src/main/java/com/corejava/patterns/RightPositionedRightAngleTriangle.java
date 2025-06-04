2e4ea132f5291d254a8e53a87ce3765a3613df93
siddujajee <jajeesiddu2002@gmail.com>
2025-06-05 01:05:11 +0530

    triangle patterns

---
+++ src/main/java/com/corejava/patterns/RightPositionedRightAngleTriangle.java
@@ -0,0 +1,15 @@
+package com.corejava.patterns;
+
+public class RightPositionedRightAngleTriangle {
+    public static void main(String[] args) {
+        for(int i=1;i<=5;i++){
+            for(int k=1;k<=5-i+1;k++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
