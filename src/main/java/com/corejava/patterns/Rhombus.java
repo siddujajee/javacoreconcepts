7fbd093a3efbc63cff1c419fc8adf0e0a583bc6a
siddujajee <jajeesiddu2002@gmail.com>
2025-06-05 01:24:46 +0530

    rhombus pattern

--- src/main/java/com/corejava/patterns/Rhombus.java
+++ src/main/java/com/corejava/patterns/Rhombus.java
@@ -1,7 +1,16 @@
 package com.corejava.patterns;
 
-public class InvertedPyramid {
+public class Rhombus {
     public static void main(String[] args) {
+        for(int i=1;i<=5;i++){
+            for(int k=1;k<=5-i+1;k++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=2*i-1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
         for(int i=1;i<=5;i++){
             for(int k=1;k<=i;k++){
                 System.out.print(" ");
