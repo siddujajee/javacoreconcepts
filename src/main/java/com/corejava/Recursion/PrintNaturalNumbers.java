14cc1fbf58b238a7ab493978885fa0ad80667476
siddujajee <jajeesiddu2002@gmail.com>
2025-06-09 23:42:29 +0530

    optimized way to print natural numbers(without using global variable)

--- src/main/java/com/corejava/Recursion/PrintNaturalNumbers.java
+++ src/main/java/com/corejava/Recursion/PrintNaturalNumbers.java
@@ -2,12 +2,21 @@ package com.corejava.Recursion;
 
 import java.util.Scanner;
 class DisplayNaturalNumbers {
-    static int count = 1;
-    static void printNumbers(int n){
-        while (count <= n) {
-            System.out.println(count);
-            count++;
-            printNumbers(n);
+    // static int count = 1;
+    // static void printNumbers(int n){
+    //     while (count <= n) {
+    //         System.out.println(count);
+    //         count++;
+    //         printNumbers(n);
+    //     }
+    // }
+
+    static void printNumbers(int i, int n){
+        if(i > n){
+            return;
+        } else {
+            System.out.println(i);
+            printNumbers(i+1, n);
         }
     }
 }
@@ -15,6 +24,7 @@ public class PrintNaturalNumbers {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int n = scan.nextInt();
-        DisplayNaturalNumbers.printNumbers(n);
+        // DisplayNaturalNumbers.printNumbers(n);
+        DisplayNaturalNumbers.printNumbers(1, n);
     }
 }
