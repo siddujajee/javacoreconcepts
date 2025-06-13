777ed010e7c148345416d016bb06d2455c561d62
siddujajee <jajeesiddu2002@gmail.com>
2025-06-14 02:25:05 +0530

    dynamically storinfg  the elements into hash map

--- src/main/java/com/corejava/collections/hashmap/HashMapping.java
+++ src/main/java/com/corejava/collections/hashmap/HashMapping.java
@@ -8,22 +8,18 @@ public class HashMapping {
     static HashMap<Integer, Integer> hm = new HashMap<>();
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("enter the length of an array");
+        System.out.println("how many numbers you want to enter");
         int n = scan.nextInt();
-        int[] a = new int[n];
+        System.out.println("enter " + n + " numbers space seperated.");
         for(int i=0;i<n;i++){
-            a[i] = scan.nextInt();
+            int m = scan.nextInt();
+            hm.put(m, hm.getOrDefault(m, 0)+1);
         }
         
-        // prefetch using hashmap
-        
-        for(int x: a){
-            hm.put(x, hm.getOrDefault(x, 0)+1);
-        }
         System.out.println("enter number of iterations");
         int iterations = scan.nextInt();
         while(iterations > 0){
-            System.out.println("enter the integer value of which you want to check the appearance");
+            System.out.println("enter the integer value of which you want to check the apparitions");
             int ele = scan.nextInt();
             System.out.println(ele + " appears " + hm.getOrDefault(ele, 0) + " times.");
             iterations--;
