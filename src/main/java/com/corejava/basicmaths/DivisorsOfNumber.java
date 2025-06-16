6fb1c35efeb6016991c545cb60172d2dc593f5ed
siddujajee <jajeesiddu2002@gmail.com>
2025-06-16 21:57:27 +0530

    added a comment in divisiors of a number

--- src/main/java/com/corejava/basicmaths/DivisorsOfNumber.java
+++ src/main/java/com/corejava/basicmaths/DivisorsOfNumber.java
@@ -9,7 +9,7 @@ public class DivisorsOfNumber {
         for(int i = 1;i*i<=n;i++){
             if(n%i == 0){
                 System.out.print(i + " ");
-                if(n/i != i){
+                if(n/i != i){ // this is to avoid duplicate prints ex: 36/6 => 6 might get print 2 times
                     System.out.print(n/i + " ");
                 }
             }
