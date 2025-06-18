810e464140854c2b933fd83dcf0e9e77c6f01d12
siddujajee <jajeesiddu2002@gmail.com>
2025-06-19 00:13:02 +0530

    second largest in array better solution

---
+++ src/main/java/com/corejava/Arrays/SecondLargestBetterSolution.java
@@ -0,0 +1,28 @@
+package com.corejava.Arrays;
+
+import java.util.Scanner;
+
+public class SecondLargestBetterSolution {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int[] a = new int[n];
+        for(int i=0;i<a.length;i++){
+            a[i] = scan.nextInt();
+        }
+
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+        for(int x:a){
+            if(x>largest){
+                secondLargest = largest;
+                largest = x;
+            } else if(x>secondLargest && x<largest){
+                secondLargest = x;
+            }
+        }
+        System.out.println(secondLargest);
+
+        
+    }    
+}
