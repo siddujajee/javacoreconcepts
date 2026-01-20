165552dc40e558327ef1b6899334dac03dda1970
siddujajee <jajeesiddu2002@gmail.com>
2026-01-20 23:57:11 +0530

    rotate array by 90 degree

---
+++ src/main/java/com/corejava/Arrays/RotateMatrix90.java
@@ -0,0 +1,28 @@
+package com.corejava.Arrays;
+
+class RotateMatrix90Solution {
+    public int[][] rotateMatrix(int[][] matrix) {
+        int n = matrix.length;
+        int[][] result = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                result[j][n - 1 - i] = matrix[i][j];
+            }
+        }
+        return result;
+    }
+}
+
+public class RotateMatrix90 {
+    public static void main(String[] args) {
+        int[][] matrix = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+        RotateMatrix90Solution solution = new RotateMatrix90Solution();
+        int[][] result = solution.rotateMatrix(matrix);
+        for (int i = 0; i < result.length; i++) {
+            for (int j = 0; j < result[0].length; j++) {
+                System.out.print(result[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
