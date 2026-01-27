0d6adfe05ccf042a68815659aa1d8598ee7bbccf
siddujajee <jajeesiddu2002@gmail.com>
2026-01-27 22:50:16 +0530

    Day 11: Triplets of array summing upto 0 better solution

---
+++ src/main/java/com/corejava/Arrays/TripletsOfArrayBetterSolution.java
@@ -0,0 +1,29 @@
+package com.corejava.Arrays;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class TripletsOfArrayBetterSolution {
+    public static void main(String[] args) {
+        int[] nums = { 2, -2, 0, 3, -3, 5 };
+        HashSet<ArrayList<Integer>> resultSet = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            HashSet<Integer> tempSet = new HashSet<>();
+            for (int j = i + 1; j < nums.length; j++) {
+                int thirdElement = -(nums[i] + nums[j]);
+                ArrayList<Integer> tempList = new ArrayList<>();
+                if (tempSet.contains(thirdElement)) {
+                    tempList.add(nums[i]);
+                    tempList.add(nums[j]);
+                    tempList.add(thirdElement);
+                    resultSet.add(tempList);
+                }
+                tempSet.add(nums[j]);
+            }
+
+        }
+        for (ArrayList<Integer> triplet : resultSet) {
+            System.out.println(triplet);
+        }
+    }
+}
