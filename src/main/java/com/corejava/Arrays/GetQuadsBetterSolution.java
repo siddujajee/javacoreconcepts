130270bbbeaa3dd1da8ce86ed0f8a29e66f4b282
siddujajee <jajeesiddu2002@gmail.com>
2026-01-28 23:38:08 +0530

    Add GetQuadsBetterSolution class for finding quadruplets summing to target

---
+++ src/main/java/com/corejava/Arrays/GetQuadsBetterSolution.java
@@ -0,0 +1,42 @@
+package com.corejava.Arrays;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+public class GetQuadsBetterSolution {
+  public static void main(String[] args) {
+    // int[] nums = { 7, -7, 1, 2, 14, 3 }; // testcase 1
+    int[] nums = { 1, -2, 3, 5, 7, 9 }; // testcase 2
+    int target = 7;
+    List<List<Integer>> list = getQuads(nums, target);
+    System.out.println(list);
+  }
+
+  static List<List<Integer>> getQuads(int[] nums, int target) {
+    List<List<Integer>> result = new ArrayList<List<Integer>>();
+    HashSet<List<Integer>> resultSet = new HashSet<>();
+    for (int i = 0; i < nums.length; i++) {
+      HashSet<Integer> tempSet = new HashSet<>();
+      for (int j = i + 1; j < nums.length; j++) {
+        for (int k = j + 1; k < nums.length; k++) {
+          int forthElement = target - (nums[i] + nums[j] + nums[k]);
+          if (tempSet.contains(forthElement)) {
+            List<Integer> tempList = new ArrayList<Integer>();
+            tempList.add(nums[i]);
+            tempList.add(nums[j]);
+            tempList.add(nums[k]);
+            tempList.add(forthElement);
+            resultSet.add(tempList);
+          }
+          tempSet.add(nums[k]);
+        }
+      }
+    }
+
+    for (List<Integer> quad : resultSet) {
+      result.add(quad);
+    }
+    return result;
+  }
+}
