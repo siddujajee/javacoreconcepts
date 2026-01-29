0b832ad56bcb7bb5d24f213be56399788edeae94
siddujajee <jajeesiddu2002@gmail.com>
2026-01-29 23:08:55 +0530

    Day13: Add GetQuadsOptimalSolution class for finding quadruplets summing to target

---
+++ src/main/java/com/corejava/Arrays/GetQuadsOptimalSolution.java
@@ -0,0 +1,56 @@
+package com.corejava.Arrays;
+
+import java.util.*;
+import java.util.List;
+
+public class GetQuadsOptimalSolution {
+  public static void main(String[] args) {
+    // int[] nums = { 7, -7, 1, 2, 14, 3 }; // testcase 1
+    // int[] nums = { 1, -2, 3, 5, 7, 9 }; // testcase 2
+    int[] nums = { 1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5 };
+    int target = 7;
+    List<List<Integer>> list = getQuads(nums, target);
+    System.out.println(list);
+  }
+
+  static List<List<Integer>> getQuads(int[] nums, int target) {
+    List<List<Integer>> result = new ArrayList<List<Integer>>();
+    Arrays.sort(nums);
+    for (int i = 0; i < nums.length; i++) {
+      if (i > 0 && nums[i] == nums[i - 1]) {
+        continue;
+      }
+      for (int j = i + 1; j < nums.length; j++) {
+        if (j > i + 1 && nums[j] == nums[j - 1]) {
+          continue;
+        }
+        int k = j + 1;
+        int l = nums.length - 1;
+        while (k < l) {
+          int sum = nums[i] + nums[j] + nums[k] + nums[l];
+          if (sum > target) {
+            l--;
+          } else if (sum < target) {
+            k++;
+          } else {
+            List<Integer> tempList = new ArrayList<Integer>();
+            tempList.add(nums[i]);
+            tempList.add(nums[j]);
+            tempList.add(nums[k]);
+            tempList.add(nums[l]);
+            result.add(tempList);
+            k++;
+            l--;
+            while (nums[l] == nums[l + 1]) {
+              l--;
+            }
+            while (nums[k] == nums[k - 1]) {
+              k++;
+            }
+          }
+        }
+      }
+    }
+    return result;
+  }
+}
