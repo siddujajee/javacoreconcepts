87a0a4a06851c0e6e0b4951699c59d0e924eb577
siddu jajee <146925563+siddujajee@users.noreply.github.com>
2026-02-10 23:43:11 +0530

    Add recursive binary search implementation
    Implement binary search algorithm using recursion.

---
+++ src/main/java/com/corejava/searching_algoriths/BinarySearchRecursionAlgorithm.java
@@ -0,0 +1,28 @@
+public class BinarySortAlgorithm {
+  public static void main(String[] args){
+    int[] a = {3, 4, 6, 7, 8, 9, 12, 16, 17};
+		int k = 12;
+		
+		int start = 0;
+		int end = a.length-1;
+		int mid = (start + end)/2;
+    int result = beginSearch(a, k, start, end);
+		if( result == -1){
+      System.out.println("element not found");
+    } else {
+      System.out.println(result);
+    }
+  }
+
+  static int beginSearch(int[] a, int k, int start, int end){
+    int mid = (start + end)/2;
+    if(a[mid] == k){
+      return mid;
+    } else if(a[mid] < k){
+      start = mid+1;
+    } else {
+      end = mid - 1;
+    }
+    return -1;
+  }
+}
