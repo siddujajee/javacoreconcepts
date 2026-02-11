bae8a197251d3c9d57dad0c753f5deb715fdf7b3
siddu jajee <146925563+siddujajee@users.noreply.github.com>
2026-02-11 23:11:38 +0530

    Add files via upload

---
+++ src/main/java/com/corejava/searching_algoriths/LowerBound.java
@@ -0,0 +1,26 @@
+public class LowerBound{
+  public static void main(String[] args) {
+    int[] a = {3,5,8,15,19};
+    int k = 55;
+    int result = lowerBoundAlgorithm(a, k);
+    System.out.println(result);
+  }
+
+  static int lowerBoundAlgorithm(int[] a, int k) {
+    int start = 0;
+    int end = a.length-1;
+    int result = a.length;
+    while(start<=end){
+      int mid = (start + end)/2;
+      if(a[mid] == k){
+        return mid;
+      } else if(a[mid] < k){
+        start = mid+1;
+      } else {
+        end = mid-1;
+        result = mid;
+      }
+    }
+    return result;
+  }
+}
