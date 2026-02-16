bdda6ee61c912ecce90f4934efe765a0ee420793
siddujajee <jajeesiddu2002@gmail.com>
2026-02-16 23:43:06 +0530

    Add comments to MergeIntervals class for better understanding of functionality

--- src/main/java/com/corejava/Arrays/MergeIntervals.java
+++ src/main/java/com/corejava/Arrays/MergeIntervals.java
@@ -1,3 +1,10 @@
+// Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals and return an array of the non-overlapping intervals that cover all the intervals in the input.
+// You can return the intervals in any order.
+// Example 1
+
+// Input: intervals = [[1,5],[3,6],[8,10],[15,18]]
+// Output: [[1,6],[8,10],[15,18]]
+// Explanation: Intervals [1,5] and [3,6] overlap, so they are merged into [1,6].
 package com.corejava.Arrays;
 
 import java.util.ArrayList;
