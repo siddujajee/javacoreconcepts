c5bb9b8b05a498f4c1fd582b292fb22a88f72a45
siddujajee <jajeesiddu2002@gmail.com>
2026-03-08 01:05:44 +0530

    Add UpperBound class to implement upper bound search algorithm

---
+++ src/main/java/com/corejava/searching_algoriths/UpperBound.java
@@ -0,0 +1,30 @@
+package com.corejava.searching_algoriths;
+
+class UpperBound {
+    public static int upperBound(int[] nums, int x) {
+        int result = nums.length;
+        int start = 0;
+        int end = nums.length - 1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (nums[mid] == x) {
+                mid++;
+                while (nums[mid] == x) {
+                    mid++;
+                }
+                start = mid;
+            } else if (nums[mid] > x) {
+                return mid;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 1, 2, 2, 3 };
+        int x = 2;
+        System.out.println(UpperBound.upperBound(nums, x));
+    }
+}
