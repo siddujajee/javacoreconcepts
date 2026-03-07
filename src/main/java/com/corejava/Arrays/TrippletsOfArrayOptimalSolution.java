4efc6957ed9e26f5aca0e91fec30cb88b5bb47fb
siddujajee <jajeesiddu2002@gmail.com>
2026-03-07 15:02:55 +0530

    Fix infinite loop risk by changing while to if in threeSum method of TrippletsOfArrayOptimalSolutionHelper class

--- src/main/java/com/corejava/Arrays/TrippletsOfArrayOptimalSolution.java
+++ src/main/java/com/corejava/Arrays/TrippletsOfArrayOptimalSolution.java
@@ -7,7 +7,8 @@ class TrippletsOfArrayOptimalSolutionHelper {
         List<List<Integer>> result = new ArrayList<List<Integer>>();
         Arrays.sort(nums);
         for (int i = 0; i < nums.length; i++) {
-            while (i > 0 && nums[i] == nums[i - 1])
+            if (i > 0 && nums[i] == nums[i - 1]) // it has to be if not while, as it can lead to infinite loop if
+                                                 // condition is true ex: [0, 1, 1] at i = 2
                 continue;
             int j = i + 1;
             int k = nums.length - 1;
@@ -38,6 +39,8 @@ class TrippletsOfArrayOptimalSolutionHelper {
 
 public class TrippletsOfArrayOptimalSolution {
     public static void main(String[] args) {
+        // int[] nums = { 0, 0, 0 }; // most important test case(it can throw array
+        // - index out of bound exception at line while (nums[j] == nums[j - 1]))
         int[] nums = { 2, -2, 0, 3, -3, 5 };
         TrippletsOfArrayOptimalSolutionHelper helper = new TrippletsOfArrayOptimalSolutionHelper();
         List<List<Integer>> result = helper.threeSum(nums);
