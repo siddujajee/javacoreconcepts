18347aa203379207a02e3ef22a8a8a5ff96552e4
siddujajee <jajeesiddu2002@gmail.com>
2026-03-20 23:40:11 +0530

    Add PeakElementInAnArray class to implement finding a peak element in an array

---
+++ src/main/java/com/corejava/searching_algoriths/PeakElementInAnArray.java
@@ -0,0 +1,32 @@
+package com.corejava.searching_algoriths;
+
+public class PeakElementInAnArray {
+    public static void main(String[] args) {
+        // int[] a = { 1, 5, 1, 2, 1 };
+        int[] a = { 1 };
+        // int[] a = { 1, 2 };
+        System.out.println(findPeakElement(a));
+    }
+
+    static int findPeakElement(int[] nums) {
+        int start = 1;
+        int end = nums.length - 2;
+        if (nums.length == 1)
+            return 0;
+        if (nums[0] > nums[1])
+            return 0;
+        if (nums[nums.length - 1] > nums[nums.length - 2])
+            return nums.length - 1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]) {
+                return mid;
+            } else if (nums[mid] > nums[mid + 1]) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
