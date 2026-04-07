4a44e7ac633ea9b228f3c56c6e66ffe92140dffc
siddujajee <jajeesiddu2002@gmail.com>
2026-04-07 23:40:03 +0530

    add displayDoublyLinkedList method to DisplayLinkedList class for outputting elements of a doubly linked list

--- src/main/java/com/corejava/collections/LinkedListExcercise/DisplayLinkedList.java
+++ src/main/java/com/corejava/collections/LinkedListExcercise/DisplayLinkedList.java
@@ -1,8 +1,19 @@
 package com.corejava.collections.LinkedListExcercise;
 
+import com.corejava.collections.LinkedListExcercise.DoublyLinkedList.DoublyLinkedNode;
+
 public class DisplayLinkedList {
     public static void display(ListNode head) {
         ListNode currentNode = head;
+        while (currentNode != null) {
+            System.out.print(currentNode.value + " ");
+            currentNode = currentNode.next;
+        }
+        System.out.println();
+    }
+
+    public static void displayDoublyLinkedList(DoublyLinkedNode head) {
+        DoublyLinkedNode currentNode = head;
         while (currentNode != null) {
             System.out.print(currentNode.value + " ");
             currentNode = currentNode.next;
