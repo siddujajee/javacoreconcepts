a2b28fb2b5544a05ecead241973044275c97e724
siddujajee <jajeesiddu2002@gmail.com>
2026-05-21 23:50:02 +0530

    add DecimalToBinary class to implement functionality for converting decimal numbers to binary representation

---
+++ src/main/java/com/Bit_manipulation/DecimalToBinary.java
@@ -0,0 +1,25 @@
+package com.Bit_manipulation;
+
+import java.util.Scanner;
+
+public class DecimalToBinary {
+    public static StringBuilder result = new StringBuilder();
+
+    public static void main(String[] args) {
+        System.out.println("enter the number ");
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+
+        System.out.println("binary representation of " + num + " is " + convert(num));
+    }
+
+    public static StringBuilder convert(int num) {
+        if (num == 1)
+            return ((result.append(1)).reverse());
+
+        result = result.append(num % 2);
+        num = num / 2;
+        convert(num);
+        return result;
+    }
+}
